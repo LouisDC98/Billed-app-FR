0500c23f8ac3488d3b30c9b6bbcef696f65bceb3
LouisDC98 <louisdacosta98@gmail.com>
2021-07-12 17:33:54 +0200

    Add test to coverage all NewBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,6 +1,8 @@
 import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import { ROUTES } from "../constants/routes"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
@@ -85,6 +87,38 @@ describe("Given I am connected as an employee", () => {
       })
       expect(mockFunction).toHaveBeenCalled()
       expect(fileInput.classList.contains('red-border')).toBeTruthy();
+    })
+  })
+})
+
+describe("Given I am connected as an employee", () => {
+  describe("When I click on submit button", () => {
+    test("Then the function handleSubmit should be called", () => {
+      document.body.innerHTML = NewBillUI();
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        email: 'test@test.com'
+      }))
+
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore: null,
+        localStorage: window.localStorage
+      })
+
+      const form = screen.getByTestId("form-new-bill");
+      const mockFunction = jest.fn(newBill.handleSubmit)
+
+      form.addEventListener('submit', mockFunction)
+      fireEvent.submit(form)
+
+      expect(mockFunction).toHaveBeenCalled()
     })
   })
 })
