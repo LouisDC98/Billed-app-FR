ceeec4a1a37717ef1d0a449eca35c95ba80f7b87
LouisDC98 <louisdacosta98@gmail.com>
2021-07-12 16:15:09 +0200

    Add test to coverage NewBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,7 +1,6 @@
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
@@ -9,6 +8,83 @@ describe("Given I am connected as an employee", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
       //to-do write assertion
+    })
+  })
+})
+
+describe("Given I am connected as an employee", () => {
+  describe("When I fill input with good extension file", () => {
+    test("Then the file should be upload", () => {
+      document.body.innerHTML = NewBillUI();
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore: null,
+        localStorage: window.localStorage
+      })
+
+      const fileInput = screen.getByTestId("file");
+      const mockFunction = jest.fn(newBill.handleChangeFile)
+
+      fileInput.addEventListener('change', mockFunction)
+      fireEvent.change(fileInput, {
+        target: {
+          files: [new File([''], 'chucknorris.png', { type: 'image/png' })],
+        },
+      })
+      expect(mockFunction).toHaveBeenCalled()
+      expect(!fileInput.classList.contains('red-border')).toBeTruthy();
+
+      fileInput.addEventListener('change', mockFunction)
+      fireEvent.change(fileInput, {
+        target: {
+          files: [new File([''], 'chucknorris.jpeg', { type: 'image/jpeg' })],
+        },
+      })
+      expect(mockFunction).toHaveBeenCalled()
+      expect(!fileInput.classList.contains('red-border')).toBeTruthy();
+
+      fileInput.addEventListener('change', mockFunction)
+      fireEvent.change(fileInput, {
+        target: {
+          files: [new File([''], 'chucknorris.jpg', { type: 'image/jpg' })],
+        },
+      })
+      expect(mockFunction).toHaveBeenCalled()
+      expect(!fileInput.classList.contains('red-border')).toBeTruthy();
+    })
+  })
+  describe("When I fill input with wrong extension file", () => {
+    test("Then the file should not be upload", () => {
+      document.body.innerHTML = NewBillUI();
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore: null,
+        localStorage: window.localStorage
+      })
+
+      const fileInput = screen.getByTestId("file");
+
+      const mockFunction = jest.fn(newBill.handleChangeFile)
+      fileInput.addEventListener('change', mockFunction)
+      fireEvent.change(fileInput, {
+        target: {
+          files: [new File([''], 'chucknorris.gif', { type: 'image/gif' })],
+        },
+      })
+      expect(mockFunction).toHaveBeenCalled()
+      expect(fileInput.classList.contains('red-border')).toBeTruthy();
     })
   })
 })
